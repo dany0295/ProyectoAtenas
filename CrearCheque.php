f4f9ec27f54e56b0a22065f59d738c914fe48628
Gemis Daniel Guevara Villeda <gemisdguevarav@gmail.com>
2018-05-19 00:43:55 -0600

    Se modifica el archivo de crear usuario

--- CrearCheque.php
+++ CrearCheque.php
@@ -1,7 +1,7 @@
 <!--
 	Módulo de creación de Usuarios
-	Martes, 17 de abril el 2018
-	11:30 PM
+	Miércoles, 16 de mayo del 2018
+	11:20 PM
 	Gemis Daniel Guevara Villeda
 	UMG - Morales Izabal
 -->
@@ -107,43 +107,69 @@
 					<div class="container-fluid">
 						<div class="row">
 							<div class="col-xs-6 col-xs-offset-3">
-							<h1 class="text-center">Creación de usuario</h1>
+							<h1 class="text-center">Creación de cheques</h1>
 							</div>
 						</div>
 						<!-- Contenedor del ícono del Usuario -->
 						
 							<div class="Icon">
 								<!-- Icono de usuario -->
-								<span class="glyphicon glyphicon-user"></span>
+								<span class="glyphicon glyphicon-plus"></span>
 							</div>
 						
 					<!-- Nombre del usuario -->
 					<div class="row">
-						<div class="col-xs-5 col-xs-offset-1">
+						<div class="col-xs-10 col-xs-offset-1">
 							<div class="input-group input-group-lg">
 								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-user"></i></span>
 								<input type="text" class="form-control" name="NombreUsuario" placeholder="Nombre" id="NombreUsuario" aria-describedby="sizing-addon1" required>
-							</div>
-						</div>
-						<!-- Nombre de inicio de sesión del usuario -->
-						<div class="col-xs-5">
-							<div class="input-group input-group-lg">
-								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-user"></i></span>
-								<input type="text" class="form-control" name="NombreInicioSesionUsuario" placeholder="Nombre de inicio de sesión" id="NombreInicioSesionUsuario" aria-describedby="sizing-addon1" required>
 							</div>
 						</div>
 					</div>
 					<br>
 					<!-- Apellido del usuario -->
 					<div class="row">
-						<div class="col-xs-5 col-xs-offset-1">
+						<div class="col-xs-10 col-xs-offset-1">
 							<div class="input-group input-group-lg">
 								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-user"></i></span>
 								<input type="text" class="form-control" name="ApellidoUsuario" placeholder="Apellido" id="ApellidoUsuario" aria-describedby="sizing-addon1" required>
 							</div>
 						</div>
-						<!-- Contraseña del usuario -->
-						<div class="col-xs-5">
+					</div>
+					<br>
+					<!-- Teléfono del usuario -->
+					<div class="row">
+						<div class="col-xs-10 col-xs-offset-1">
+							<div class="input-group input-group-lg">
+								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-earphone"></i></span>
+								<input type="tel" class="form-control" name="TelefonoUsuario" placeholder="Teléfono" id="TelefonoUsuario" aria-describedby="sizing-addon1" required>
+							</div>
+						</div>
+					</div>
+					<br>
+					<!-- Dirección del usuario -->
+					<div class="row">
+						<div class="col-xs-10 col-xs-offset-1">
+							<div class="input-group input-group-lg">
+								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-home"></i></span>
+								<input type="text" class="form-control" name="DireccionUsuario" placeholder="Dirección" id="DireccionUsuario" aria-describedby="sizing-addon1" required>
+							</div>
+						</div>
+					</div>
+					<br>
+					<!-- Correo del usuario -->
+					<div class="row">
+						<div class="col-xs-10 col-xs-offset-1">
+							<div class="input-group input-group-lg">
+								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-envelope"></i></span>
+								<input type="email" class="form-control" name="CorreoUsuario" placeholder="Correo" id="CorreoUsuario" aria-describedby="sizing-addon1" required>
+							</div>
+						</div>
+					</div>
+					<br>
+					<!-- Contraseña del usuario -->
+					<div class="row">
+						<div class="col-xs-10 col-xs-offset-1">
 							<div class="input-group input-group-lg">
 								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-lock"></i></span>
 								<input type="password" class="form-control" name="PasswordUsuario" placeholder="Contraseña" id="PaswordUsuario" aria-describedby="sizing-addon1" required>
@@ -151,16 +177,9 @@
 						</div>
 					</div>
 					<br>
-					<!-- Teléfono del usuario -->
+					<!-- Repetición de contraseña del usuario -->
 					<div class="row">
-						<div class="col-xs-5 col-xs-offset-1">
-							<div class="input-group input-group-lg">
-								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-earphone"></i></span>
-								<input type="tel" class="form-control" name="TelefonoUsuario" placeholder="Teléfono" id="TelefonoUsuario" aria-describedby="sizing-addon1" required>
-							</div>
-						</div>
-						<!-- Repetición de contraseña del usuario -->
-						<div class="col-xs-5">
+						<div class="col-xs-10 col-xs-offset-1">
 							<div class="input-group input-group-lg">
 								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-lock"></i></span>
 								<input type="password" class="form-control" name="RePasswordUsuario" placeholder="Ingrese nuevamente la contraseña" id="RePaswordUsuario" aria-describedby="sizing-addon1" required>
@@ -168,67 +187,25 @@
 						</div>
 					</div>
 					<br>
-					<!-- Dirección del usuario -->
+					<!-- Rol del usuario
 					<div class="row">
-						<div class="col-xs-5 col-xs-offset-1">
-							<div class="input-group input-group-lg">
-								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-home"></i></span>
-								<input type="text" class="form-control" name="DireccionUsuario" placeholder="Dirección" id="DireccionUsuario" aria-describedby="sizing-addon1" required>
-							</div>
-						</div>
-						<!-- Rol del usuario -->
-						<div class="col-xs-4">
+						<div class="col-xs-10 col-xs-offset-1">
 							<div class="input-group input-group-lg">
 								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-lock"></i></span>
 								<select class="form-control" name="RolUsuario" id="RolUsuario">
-									<option value="" disabled selected>Selecciona el rol del usuario</option>
-										<option value="Administrador">Administrador</option>
-										<option value="Auditor">Auditor</option>
-										<option value="Gerente">Gerente</option>
-										<option value="Pagador">Pagador</option>
-								</select>
-							</div>
-						</div>
-						<!-- Button trigger modal -->
-						<div class="col-xs-1">
-							<div class="input-group input-group-lg">
-								<button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#ModalCrearUsuario">+</button>
-							</div>
-						</div>
-					</div>
-					<br> 
-					<!-- Correo del usuario -->
-					<div class="row">
-						<div class="col-xs-5 col-xs-offset-1">
-							<div class="input-group input-group-lg">
-								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-envelope"></i></span>
-								<input type="email" class="form-control" name="CorreoUsuario" placeholder="Correo" id="CorreoUsuario" aria-describedby="sizing-addon1" required>
-							</div>
-						</div>
-						<!-- Grupo del usuario -->
-						<div class="col-xs-4">
-							<div class="input-group input-group-lg">
-								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-lock"></i></span>
-								<select class="form-control" name="RolUsuario" id="RolUsuario">
-								<option value="" disabled selected>Selecciona el grupo al que pertenece el usuario</option>
+								<option value="" disabled selected>Selecciona el rol del usuario</option>
 									<option value="Administrador">Administrador</option>
-									<option value="Auditor">Auditoría</option>
-									<option value="Gerente">Gerencia</option>
-									<option value="Pagador">Pagos</option>
+									<option value="Auditor">Auditor</option>
+									<option value="Gerente">Gerente</option>
+									<option value="Pagador">Pagador</option>
 								</select>
 							</div>
 						</div>
-						<!-- Button trigger modal -->
-						<div class="col-xs-1">
-							<div class="input-group input-group-lg">
-								<button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#ModalCrearGrupo">+</button>
-							</div>
-						</div>
 					</div>
-					<br>
+					<br> -->
 					<!-- Puesto del usuario -->
 					<div class="row">
-						<div class="col-xs-4 col-xs-offset-1">
+						<div class="col-xs-10 col-xs-offset-1">
 							<div class="input-group input-group-lg">
 								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-lock"></i></span>
 								<select class="form-control" name="PuestoUsuario" id="PuestoUsuario">
@@ -246,31 +223,6 @@
 								</select>
 							</div>
 						</div>
-						<!-- Button trigger modal -->
-						<div class="col-xs-1">
-							<div class="input-group input-group-lg">
-								<button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#ModalCrearPuesto">+</button>
-							</div>
-						</div>
-						<!-- Rango de cheques que creará el usuario -->
-						<div class="col-xs-4">
-							<div class="input-group input-group-lg">
-								<span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-lock"></i></span>
-								<select class="form-control" name="RolUsuario" id="RolUsuario">
-								<option value="" disabled selected>Selecciona el rango monetario con que podrá operar el usuario</option>
-									<option value="Administrador">0 - 1,000</option>
-									<option value="Auditor">1,001 - 10,000</option>
-									<option value="Gerente">10,001 - 25,000</option>
-									<option value="Pagador">25,001 - 50,000</option>
-								</select>
-							</div>
-						</div>
-						<!-- Button trigger modal -->
-						<div class="col-xs-1">
-							<div class="input-group input-group-lg">
-								<button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">+</button>
-							</div>
-						</div>
 					</div>
 					<br>
 					<!-- Resgistrar -->
@@ -284,94 +236,8 @@
 							</div>
 						</div>
 					</div>
+					<br>
 				</div>
-				<!-- Modal para crear puesto -->
-				<div class="modal fade slide left" id="ModalCrearPuesto" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-				  <div class="modal-dialog" role="document">
-					<div class="modal-content">
-					  <div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-
-						</button>
-						<h1 class="modal-title" id="myModalLabel">Crear nuevo puesto</h1>
-
-					  </div>
-					  <div class="modal-body">
-						<p class="lead">Ingrese los datos</p>
-						<form method="post" id="myForm">
-						  <div class="form-group">
-							<label for="name">Codigo del puesto</label>
-							<input type="text" name="CodigoPuesto" id="CodigoPuesto" class="form-control" placeholder="Codigo de puesto" value="" required/>
-						  </div>
-						  <div class="form-group">
-							<label for="email">Nombre del puesto</label>
-							<input type="text" name="NombreNuevoPuesto" id="NombreNuevoPuesto" class="form-control" placeholder="Nombre del puesto" value="" required/>
-						  </div>
-						  <input type="submit" name="GuardarPuesto" class="btn btn-success btn-lg" value="Crear">
-						</form>
-					  </div>
-					  <div class="modal-footer">
-						<button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-					  </div>
-					</div>
-				  </div>
-				</div>
-				<!-- 
-				<?php
-					include_once "Seguridad/conexion.php";
-					// Código php para el modal de creación de puesto
-					if (isset($_POST['GuardarPuesto'])) {
-						echo "<script languaje='javascript'>
-							alert('Guardar puesto');
-							</script>";
-					}
-					// Código php para el modal de creación de puesto
-					if (isset($_POST['GuardarGrupo'])) {
-						echo "<script languaje='javascript'>
-							alert('Guardar Grupo');
-							</script>";
-					}					
-				?> 
-				
-				<!-- Modal para crear Grupo al que pertenece -->
-				<div class="modal fade slide left" id="ModalCrearGrupo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-				  <div class="modal-dialog" role="document">
-					<div class="modal-content">
-					  <div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-
-						</button>
-						<h1 class="modal-title" id="myModalLabel">Crear nuevo grupo</h1>
-
-					  </div>
-					  <div class="modal-body">
-						<p class="lead">Ingrese los datos</p>
-						<form method="post" id="myForm">
-						  <div class="form-group">
-							<label for="name">Codigo del grupo</label>
-							<input type="text" name="CodigoGrupo" id="CodigoGrupo" class="form-control" placeholder="Codigo de grupo" value="" required/>
-						  </div>
-						  <div class="form-group">
-							<label for="email">Nombre del grupo</label>
-							<input type="text" name="NombreNuevoGrupo" id="NombreNuevoGrupo" class="form-control" placeholder="Nombre del grupo" value="" required/>
-						  </div>
-						  <div class="form-group">
-							<label for="email">Rango del grupo</label>
-							<input type="text" name="RangoNuevoGrupo" id="RangoNuevoGrupo" class="form-control" placeholder="Rango del grupo" value="" required/>
-						  </div>
-						  <div class="form-group">
-							<label for="email">Puesto del grupo</label>
-							<input type="text" name="PuestoNuevoGrupo" id="PuestoNuevoGrupo" class="form-control" placeholder="Puesto del grupo" value="" required/>
-						  </div>
-						  <input type="submit" name="GuardarGrupo" class="btn btn-success btn-lg" value="Crear">
-						</form>
-					  </div>
-					  <div class="modal-footer">
-						<button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-					  </div>
-					</div>
-				  </div>
-				</div>			
 				<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 				<script src="js/jquery-1.11.3.min.js"></script>
 
